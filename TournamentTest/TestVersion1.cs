7af55a07cec38e53c3c0c15f4d7601beb8c510bf
Sebastian Thorup Frederiksen <sthorupf@hotmail.com>
2018-10-24 11:32:44 +0200

    test

--- TournamentTest/TestVersion1.cs
+++ TournamentTest/TestVersion1.cs
@@ -5,11 +5,59 @@ using TournamentLib;
 namespace TournamentTest
 {
     [TestClass]
-    public class TestVersion1
+    public class DragonsLairTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        Tournament currentTournament;
+
+        [TestInitialize]
+        public void SetupForTest()
         {
+            currentTournament = new Tournament("Vinter Turnering");
+        }
+
+        [TestMethod]
+        public void TournamentHasEvenNumberOfTeams()
+        {
+            int numberOfTeams = currentTournament.GetTeams().Count;
+            Assert.AreEqual(0, numberOfTeams % 2);
+        }
+
+        [TestMethod]
+        public void EqualNumberOfWinnersAndLosersPerRound()
+        {
+            int numberOfRounds = currentTournament.GetNumberOfRounds();
+            for (int round = 0; round < numberOfRounds; round++)
+            {
+                Round currentRound = currentTournament.GetRound(round);
+                int numberOfWinningTeams = currentRound.GetWinningTeams().Count;
+                int numberOfLosingTeams = currentRound.GetLosingTeams().Count;
+                Assert.AreEqual(numberOfWinningTeams, numberOfLosingTeams);
+            }
+        }
+
+        [TestMethod]
+        public void OneWinnerInLastRound()
+        {
+            // Verifies there is exactly one winner in last round
+            int numberOfRounds = currentTournament.GetNumberOfRounds();
+            Round currentRound = currentTournament.GetRound(numberOfRounds - 1);
+            int numberOfWinningTeams = currentRound.GetWinningTeams().Count;
+            Assert.AreEqual(1, numberOfWinningTeams);
+        }
+
+        [TestMethod]
+        public void AllMatchesInPreviousRoundsFinished()
+        {
+            bool matchesFinished = true;
+            int numberOfRounds = currentTournament.GetNumberOfRounds();
+            for (int round = 0; round < numberOfRounds; round++)
+            {
+                Round currentRound = currentTournament.GetRound(round);
+                if (currentRound.IsMatchesFinished() == false)
+                    matchesFinished = false;
+            }
+            Assert.AreEqual(true, matchesFinished);
         }
     }
+
 }
