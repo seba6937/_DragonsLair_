f35dc864483a60c776cd3ee8722daea949de890e
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-10-24 12:14:49 +0200

    Added getMatch and ismatchesfinished

--- TournamentLibrary/Round.cs
+++ TournamentLibrary/Round.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TournamentLib
 {
@@ -13,14 +14,27 @@ namespace TournamentLib
 
         public Match GetMatch(string teamName1, string teamName2)
         {
-            // TODO: Implement this method
+            foreach (Match mac in matches)
+            {
+                if (mac.FirstOpponent.ToString() == teamName1 && mac.SecondOpponent.ToString() == teamName2)
+                {
+                    return mac;
+                }
+            }
             return null;
         }
 
         public bool IsMatchesFinished()
         {
-            // TODO: Implement this method
-            return false;
+            foreach (Match mac in matches)
+            {
+                if(mac.Winner == null)
+                {
+                    return false;
+                } 
+            }
+            return true;
+            
         }
 
         public List<Team> GetWinningTeams()
